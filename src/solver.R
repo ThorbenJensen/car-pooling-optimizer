a5ef10cc8a05da2521ea666e066ed2d4b8e57256
Thorben Jensen <jensen.thorben@gmail.com>
2018-11-22 21:53:38 +0100

    finalized prototype of solver

--- src/solver.R
+++ src/solver.R
@@ -3,34 +3,37 @@
 library(lpSolveAPI)
 library(assertthat)
 
+sprintf_ <- function(...) print(sprintf(...))
 
-# SOLUCTION SPACE ----
-lprec <- make.lp(nrow = 2, ncol = 4 + 4)
-set.type(lprec, columns = 1:8, type = "binary")
-
-
-# DATA ----
-budget <- c(19, 19, 0, 1)
-cap_total <- c(2, 2, 2, 2)
-cap_other <- cap_total - 1
-
-dist_join <- 0.6
-dist <- c(1, 1, dist_join, dist_join)
-dist_full <- dist == max(dist)
+# INPUT DATA ----
+N <- 5
+J <- 2
+budget <- c(19, 18, 0, 1, 5)
+no_car <- c(0, 0, 0, 1, 1)
+cap_total <- c(3, 3, 5, 3, 4)
 
 W <- matrix(c(1, 0,
-              1, 0,
-              1, 0,
-              1, 0), nrow = 4, byrow = TRUE)
+              0, 1,
+              0, 1,
+              0, 1,
+              0, 1), nrow = N, byrow = TRUE)
 
 H <- matrix(c(1, 0,
               1, 0,
-              1, 0,
-              1, 0), nrow = 4, byrow = TRUE)
+              0, 1,
+              0, 1,
+              1, 0), nrow = N, byrow = TRUE)
 
-
-# VALIDATE DATA ----
-# matrices W and H
+# VALIDATE INPUT DATA ----
+# consistent length of input data
+assert_that(length(budget) == N, msg = "inconsistent length.")
+assert_that(length(no_car) == N, msg = "inconsistent length.")
+assert_that(length(cap_total) == N, msg = "inconsistent length.")
+assert_that(nrow(W) == N, msg = "inconsistent length.")
+assert_that(nrow(H) == N, msg = "inconsistent length.")
+# same numver of columns between matrices
+assert_that(ncol(W) == ncol(H), msg = "inconsistent length.")
+# matrices W and H have consitent row sums
 assert_that(all(apply(W, 1, sum) %in% c(0, 1)),
             msg = "Row sum incorrect.")
 assert_that(all(apply(H, 1, sum) %in% c(0, 1)),
@@ -38,45 +41,26 @@ assert_that(all(apply(H, 1, sum) %in% c(0, 1)),
 assert_that(all(apply(W, 1, sum) == apply(H, 1, sum)),
             msg = "Row sums inconsistent")
 
+# SOLUTION SPACE ----
+lprec <- make.lp(nrow = J, ncol = N)
+set.type(lprec, columns = 1:N, type = "binary")
 
 # CONSTRAINTS ----
-# for last strech of journey:
 # per time slot, total_capacity of drivers at least the number of drivers
-add.constraint(lprec,
-               W[, 1] * cap_total, '>=', sum(W[, 1]), indices = 1:4)
-add.constraint(lprec,
-               W[, 2] * cap_total, '>=', sum(W[, 2]), indices = 1:4)
-add.constraint(lprec,
-               H[, 1] * cap_total, '>=', sum(H[, 1]), indices = 1:4)
-add.constraint(lprec,
-               H[, 2] * cap_total, '>=', sum(H[, 2]), indices = 1:4)
-# for first strech of journey:
-# per time slot, enough capacity for those with full distance
-add.constraint(lprec,
-               W[, 1] * cap_total, '>=', sum(W[, 1]),
-               indices = 5:8)
-add.constraint(lprec,
-               W[, 2] * cap_total, '>=', sum(W[, 2]),
-               indices = 5:8)
-add.constraint(lprec,
-               H[, 1] * cap_total, '>=', sum(H[, 1]),
-               indices = 5:8)
-add.constraint(lprec,
-               H[, 2] * cap_total, '>=', sum(H[, 2]),
-               indices = 5:8)
-# late joiners cannot drive first strech
-add.constraint(lprec, rep(1, 2), '=', 0, indices = 3:4)
-# if not a late joiner, driving second strech requires driving the first one
-add.constraint(lprec, c(1, -1), '>=', 0, indices = c(1, 5))
-add.constraint(lprec, c(1, -1), '>=', 0, indices = c(2, 6))
+cap_other <- cap_total - 1
+for (A in list(W, H)) {
+  for (j in 1:J) {
+    add.constraint(lprec, A[, j] * cap_total, '>=', sum(A[, j]))
+  }
+}
+# only who has a car can be driver
+add.constraint(lprec, no_car, '=', 0)
 
 
 # OBJECTIVE FUNCTION ----
 lp.control(lprec, sense = "min")
-fair_w <- 0.1
-set.objfn(lprec,
-          c(dist - dist_join, rep(dist_join, 4)) +
-          c(fair_w * budget, fair_w * budget))
+fairness_weight <- 0.1
+set.objfn(lprec, rep(1, N) + c(fairness_weight * budget))
 
 # SOLVE ----
 lprec
@@ -84,3 +68,30 @@ solve(lprec)
 get.objective(lprec)
 get.constraints(lprec)
 get.variables(lprec)
+
+# MAKE SCHEDULE ---
+all_drivers <- get.variables(lprec)[1:N]
+sprintf_("Total number of drivers: %i", length(all_drivers[all_drivers == 1]))
+
+for (A in list(W, H)) {
+  for (j in 1:J) {
+
+    # get drivers
+    drivers <- which(all_drivers & A[, j])
+    if (length(drivers) == 0) next
+    direction <- ifelse(all(A == W), "work", "home")
+    sprintf_("Towards '%s' at time slot %i:", direction, j)
+    sprintf_("Drivers: %s", paste(drivers, collapse = " "))
+    # get passengers
+    passengers <- setdiff(which(A[, j] == 1), drivers)
+    if (length(passengers) == 0) next
+    # test that there is enough capacity
+    assert_that(sum(cap_other[drivers]) >= length(passengers),
+                msg = "no capacity.")
+    # assign passengers to drivers
+    cap_pool <- rep(drivers, cap_other[drivers])
+    assignment <- sample(cap_pool, length(passengers), replace = FALSE)
+    # report
+    sprintf_("Passenger %s rides with driver %s", passengers, assignment)
+  }
+}
